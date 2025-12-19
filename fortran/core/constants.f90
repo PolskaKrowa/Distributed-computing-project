1b98a5926f4297bf73ab41c078e5010459af0304
Steve <mentalabuse2@gmail.com>
2025-12-19 18:33:08 +0000

    add pi to constants
    added pi value to fortran shared constants

--- fortran/core/constants.f90
+++ fortran/core/constants.f90
@@ -3,10 +3,11 @@ module constants
   implicit none
   private
 
-  public :: zero_r64, one_r64, two_r64
+  public :: zero_r64, one_r64, two_r64, pi_r64
 
   real(r64), parameter :: zero_r64 = 0.0_r64
   real(r64), parameter :: one_r64  = 1.0_r64
   real(r64), parameter :: two_r64  = 2.0_r64
+  real(r64), parameter :: pi_r64   = 3.14159265358979323843_r64
 
 end module constants
